a312e8b229d469e9bcb2a99e72ed6a6ec2b25461
Quant Developer <user@example.com>
2026-08-12 09:38:47 +0800

    chore: remove build/test artifacts from git tracking (maestro bin/lib, coverage, logs, e2e screenshots)

--- .maestro/maestro.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal
-set "JAVA_HOME=C:\Program Files\Microsoft\jdk-17.0.19.10-hotspot"
-set "PATH=%~dp0bin;%PATH%"
-set "MAESTRO_CLI_NO_ANALYTICS=1"
-set "MAESTRO_CLI_ANALYSIS_NOTIFICATION_DISABLED=1"
-maestro.bat %*
